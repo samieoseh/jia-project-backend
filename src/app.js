f0f0da4d8b4b5f50252db608afc1e1ce6d0d06c4
samieIzone5 <samuel@izone5.com.ng>
2024-10-23 11:56:20 +0000

    added cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ require('./db/dbconnection')
 const express = require("express");
 const userRouter = require('./routers/user');
 const User = require('./models/user');
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors())
 app.use(userRouter)
 
 app.listen(port, () => {
